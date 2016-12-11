4eb2435965879102340ee8d74e70f6622f108318
Claudio Mezzasalma <claudio.mezzasalma@gmail.com>
2016-12-11 12:13:09 +0100

    Added LCD management

--- smart-aquarium.py
+++ smart-aquarium.py
@@ -1,17 +1,25 @@
+#!/usr/bin/python3
+
 from pubnub import Pubnub
 from w1thermsensor import W1ThermSensor
 import time
+from RPLCD import CharLCD
+from os.path import dirname, abspath
 
 # init temperature probe library
 sensor = W1ThermSensor()
 # init Pubnum keys
-keys_file = "conf/pubnub.keys"
+keys_file = dirname(abspath(__file__)) + "/conf/pubnub.keys"
 keys = {}
 # Pubnub channel
 channel = "smart-aquarium"
 # Reading interval in seconds
 sleep_interval = 5
-
+''' 
+init LCD. Pinout configuration based on 
+https://cdn-learn.adafruit.com/downloads/pdf/drive-a-16x2-lcd-directly-with-a-raspberry-pi.pdf
+'''
+lcd = CharLCD(cols=16, rows=2, pin_rs=22, pin_rw=None, pin_e=18, pins_data=[16, 11, 40, 15])
 # Read Pubnub's keys from conf/pubnub.keys
 with open(keys_file) as f:
     for line in f:
@@ -41,7 +49,11 @@ def disconnect(message):
 # Read every N seconds and publish to Pubnub
 while (True):
     temperature = {}
-    temperature["value"] = sensor.get_temperature()
-    temperature["date"] = time.strftime("%Y-%m-%d %H:%M:%S")
+    temp_value = sensor.get_temperature()
+    temp_date_pubnub = time.strftime("%Y-%m-%d %H:%M:%S")
+    temp_date_lcd = time.strftime("%Y-%m-%d %H:%M")
+    temperature["value"] = temp_value
+    temperature["date"] = temp_date_pubnub
     pubnub.publish(channel, temperature, error=error)
+    lcd.write_string("Temp: " + str(temp_value) +  "\r\n" + temp_date_lcd)
     time.sleep(sleep_interval)
